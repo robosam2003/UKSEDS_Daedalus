7ae9d5bf6f8b979235e487b43b7546fd4fd75acd
robos <robosam2003@gmail.com>
2022-03-31 21:06:06 +0100

    now using SDIO on the sdfat library much quicker if you do it right, now takes only ~1.5ms max per 512 byte buffer write. Looking into preallocating and writing to a single page each time.

--- code/SDcardDataLog/src/main.cpp
+++ code/SDcardDataLog/src/main.cpp
@@ -4,11 +4,16 @@
 
 #include <Arduino.h>
 #include <SPI.h>
-#include <SD.h>
+#include "SdFat.h"
 
 
-const int chipSelect = BUILTIN_SDCARD;
+SdFat32 sd;
+File32 file;
+const int chipSelect = 254;
 elapsedMicros timer1;
+
+const size_t BUF_DIM = 512;
+uint8_t buf[BUF_DIM];
 
 struct dataStruct {
     uint16_t acc_rawx;
@@ -17,60 +22,51 @@ struct dataStruct {
 };
 
 
-//prototypes
-void logData();
-
-
 void setup() {
 // write your initialization code here
     delay(3000);
     Serial.begin(9600);
-    pinMode(LED_BUILTIN, OUTPUT);
-    //pinMode(button1, INPUT);
     while (!Serial);
-    //attachInterrupt(digitalPinToInterrupt(button1), logData, RISING);
-
     // SD card initialisation
-    if (!SD.begin(chipSelect)) {
+    if (!sd.begin(SdioConfig(FIFO_SDIO))) {
         Serial.println("Could not mount SD card");
-        digitalWrite(LED_BUILTIN, HIGH);
         while (true);
     }
     Serial.println("Card initialised");
 }
 
-File SDCardInit(){
-    /// use O_CREAT | O_WRITE for faster write times :)
-    File dataFile = SD.open("dataLog1.dat", O_CREAT | O_WRITE);
-    return dataFile;
+
+void logData(byte arr[512]) {  /// SDFat has a 512 byte buffer, so we only need to flush after 512 bytes have been written to the buffer
+    if (!file.write((const uint8_t*)&arr, 512)) {
+        Serial.println("Error: write halted");
+    }
 }
 
-void logData(File dataFile, struct dataStruct data) {  /// SDFat has a 512 byte buffer, so we only need to flush after 512 bytes have been written to the buffer
-    if (dataFile) {
-        dataFile.write((const uint8_t *)&data, sizeof(data));
-    }
-    else{
-        Serial.println("Some error has occured");
+void logData(struct dataStruct data) {  /// SDFat has a 512 byte buffer, so we only need to flush after 512 bytes have been written to the buffer
+    if (!file.write((const uint8_t*)&data, sizeof(data))) {
+        Serial.println("Error: write halted");
     }
 }
 
 void loop() {
-    struct dataStruct mydata = {0,0,0};
-    File dataFile = SDCardInit();
-    for (int i=0;i<10;i++) {
-        mydata.acc_rawx = i;
-        mydata.acc_rawy = i+1;
-        mydata.acc_rawz = i+2;
-        unsigned long a = micros();
-        logData(dataFile, mydata);
-        unsigned long b = micros();
-        Serial.println(b-a);
+    if (!file.open("dataLog1.txt", O_CREAT | O_WRITE)) {
+        Serial.println("Could not open file!");
     }
-    unsigned long beforeFlush = micros();
-    dataFile.flush();
-    unsigned long afterFlush = micros();
-    Serial.printf("To flush 512 bytes it takes: %d\n", afterFlush-beforeFlush);
-    dataFile.close();
+    byte byteArr[512];
+    for (int a=0;a<512;a++){ byteArr[a] = a;}
+    for(int j=0;j<10;j++) {
+        unsigned long a = micros();
+        logData(byteArr); // 512 bytes
+        unsigned long b = micros();
+        Serial.println(b - a);
+
+        unsigned long beforeFlush = micros();
+        file.flush();
+        unsigned long afterFlush = micros();
+        Serial.printf("To flush 512 bytes it takes: %d\n", afterFlush - beforeFlush);
+        Serial.printf("Total time %d\n", afterFlush - a);
+    }
+    file.close();
     delay(3000);
 
 
