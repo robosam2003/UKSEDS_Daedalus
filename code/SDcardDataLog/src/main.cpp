fd7ab53ac8742522b4a9b0c8bf10d530fb35024e
robos <robosam2003@gmail.com>
2022-04-04 10:06:39 +0100

    Still trying to achieve faster write speeds, prealloacting makes it 2-3ms faster

--- code/SDcardDataLog/src/main.cpp
+++ code/SDcardDataLog/src/main.cpp
@@ -36,16 +36,16 @@ void setup() {
         while (true);
     }
 
-    if (!file.open("dataLog11.bin", O_CREAT | O_WRITE)) {
+    if (!file.open("dataLog16.csv", O_CREAT | O_WRITE)) {
         Serial.println("Could not open file!");
     }
     // File must be pre-allocated to avoid huge
     // delays searching for free clusters.
-    /*if (!file.preAllocate(LOG_FILE_SIZE)) { // if i dont preallocate, there;s an extra 3-5ms delay in writing
+    if (!file.preAllocate(LOG_FILE_SIZE)) { // if i dont preallocate, there;s an extra 3-5ms delay in writing
         Serial.println("preAllocate failed\n");
         file.close();
         return;
-    }*/
+    }
     Serial.println("Card initialised");
 }
 
@@ -56,26 +56,28 @@ void logData(byte arr[512]) {  /// SDFat has a 512 byte buffer, so we only need 
     }
     for(int i=0;i<512;i++) { file.printf("%d,",arr[i]); }
     file.printf("\n");*/
-    if (!file.write(arr, 512));
+    if (!file.write(arr, 512)) {
+        Serial.print("Error: could not write buffer");
+    }
 }
 
 void logData(struct dataStruct data) {  /// SDFat has a 512 byte buffer, so we only need to flush after 512 bytes have been written to the buffer
-    if (!file.printf("%d,%d,%d,", data.acc_rawx, data.acc_rawy, data.acc_rawz)) {
+    if (!file.printf("%d,%d,%d,\n", data.acc_rawx, data.acc_rawy, data.acc_rawz)) {
 
         Serial.println("Error: write halted");
     }
-    for (int i=0;i<464;i++) { file.printf("%d,", data.arr[i]); } // TODO: How do we write a struct to a page of flash like joeyB said???? - Tha would be mega fast.
-    file.printf("\n");
+    //for (int i=0;i<464;i++) { file.printf("%d,", data.arr[i]); } // TODO: How do we write a struct to a page of flash like joeyB said???? - That would be mega fast.
+    //file.printf("\n");
 }
 
 void loop() {
 
     byte byteArr[512] = {1, 2, 3, 5, 6, 7, 8, 9, 255, 13};
     //for (int a=0;a<512;a++){ byteArr[a] = a;}
-    //struct dataStruct mydata = {1,2,32, {3}};
+    struct dataStruct mydata = {1,2,32};
     while(1){
-        unsigned long a = micros();
-        logData(byteArr); // 512 bytes
+        unsigned long a = micros(); // IS SERIAL WRITE SLOWING IT DOWN????
+        logData(mydata); // 512 bytes
         unsigned long b = micros();
         Serial.println(b - a);
 
@@ -84,7 +86,7 @@ void loop() {
         unsigned long afterFlush = micros();
         Serial.printf("To flush 512 bytes it takes: %d\n", afterFlush - beforeFlush);
         Serial.printf("Total time %d\n", afterFlush - a);
-        delay(10);
+        //delay(1);
     }
     //file.close();
 
