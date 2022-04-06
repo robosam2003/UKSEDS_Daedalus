5cb54b6f329f2cf59f308fd99b3ec768beab5fa2
robos <robosam2003@gmail.com>
2022-04-06 21:52:56 +0100

    MEGA SUCCESS! - Now only takes ~411us until it is ready to transmit again -_- It works and I don't know why.

--- code/RFM96W_Transmitter_FSK/src/main.cpp
+++ code/RFM96W_Transmitter_FSK/src/main.cpp
@@ -1,13 +1,7 @@
-
-// include the library
 #include <Arduino.h>
 #include <RadioLib.h>
 
-
-elapsedMicros microTimer;
-
-
-// SX1278 has the following connections:
+// RFM96W has the following connections:
 // NSS pin:   10
 // DIO0 pin:  2
 // RESET pin: 9
@@ -20,7 +14,7 @@ void setFlag();
 int transmissionState = RADIOLIB_ERR_NONE;
 
 void setup() {
-    Serial.begin(500000);
+    Serial.begin(115200);
 
     // initialize SX1278 with default settings
     Serial.print(F("[SX1278] Initializing ... "));
@@ -72,67 +66,40 @@ void setFlag() {
     if(!enableInterrupt) {
         return;
     }
-
     // we sent a packet, set the flag
     transmittedFlag = true;
 }
 
-void transmitData(byte arr[64]) { // TODO: This needs speeding up, currently takes over 7ms - not good.
+void transmitData(byte arr[]) {
     // check if the previous transmission finished
-    while (!transmittedFlag);
+    while (!transmittedFlag) {};
     if(transmittedFlag) {
         // disable the interrupt service routine while
         // processing the data
         enableInterrupt = false;
-
         // reset flag
         transmittedFlag = false;
-
         if (transmissionState == RADIOLIB_ERR_NONE) {
             // packet was successfully sent
-            //if (Serial) { Serial.println("transmission finished!");}
-
-            // NOTE: when using interrupt-driven transmit method,
-            //       it is not possible to automatically measure
-            //       transmission data rate using getDataRate()
-
+            if (Serial) { Serial.println("transmission finished!");}
         } else {
             if (Serial) { Serial.printf("failed, code ");}
             if (Serial) { Serial.println(transmissionState);}
-
         }
 
-        // NOTE: in FSK mode, SX127x will not automatically
-        //       turn transmitter off after sending a packet
-        //       set mode to standby to ensure we don't jam others
-        //radio.standby()
+        /// TRANSMIT DATA
 
-        // wait a second before transmitting again
-        delay(5);
-
-        // send another one
-        //if (Serial) { Serial.printf("[RFM96W] Sending another packet ... ");}
-
-        // you can transmit C-string or Arduino string up to
-        // 256 characters long
-        //transmissionState = radio.startTransmit("Hello World! THIS IS SAM");
-
-        // you can also transmit byte array up to 256 bytes long
-
-        byte byteArr[63] = {};
-        for (int i=0; i<63; i++) { byteArr[i] = arr[i]; }
-
-        int state = radio.startTransmit(byteArr, 63); //
-        if (state == RADIOLIB_ERR_NONE) {
-            //if (Serial) { Serial.println("[RFM96W] Packet transmitted successfully!");}
-        } else if (state == RADIOLIB_ERR_PACKET_TOO_LONG) {
-            if (Serial) { Serial.println(F("[RFM96W] Packet too long!"));}
-        } else if (state == RADIOLIB_ERR_TX_TIMEOUT) {
-            if (Serial) { Serial.println(F("[RFM96W] Timed out while transmitting!"));}
-        } else {
-            if (Serial) { Serial.println(F("[RFM96W] Failed to transmit packet, code "));}
-            if (Serial) { Serial.println(state);}
-        }
+        transmissionState = radio.startTransmit(arr, 63);
+//        if (state == RADIOLIB_ERR_NONE) {
+//            if (Serial) { Serial.println("[RFM96W] Packet transmitted successfully!");}
+//        } else if (state == RADIOLIB_ERR_PACKET_TOO_LONG) {
+//            if (Serial) { Serial.println(F("[RFM96W] Packet too long!"));}
+//        } else if (state == RADIOLIB_ERR_TX_TIMEOUT) {
+//            if (Serial) { Serial.println(F("[RFM96W] Timed out while transmitting!"));}
+//        } else {
+//            if (Serial) { Serial.println(F("[RFM96W] Failed to transmit packet, code "));}
+//            if (Serial) { Serial.println(state);}
+//        }
         // we're ready to send more packets,
         // enable interrupt service routine
         enableInterrupt = true;
@@ -149,6 +116,5 @@ void loop() {
     unsigned long b = micros();
     if (Serial) { Serial.printf("Transmission took %d (us)\n", b-a); }
     counter++;
-    delay(10);
-
+    delayMicroseconds(10000-(b-a));
 }
