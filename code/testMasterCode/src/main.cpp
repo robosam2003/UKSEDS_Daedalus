b28caad4b74dbb6474841b6cf88cd6a9e4db5c84
robos <robosam2003@gmail.com>
2022-04-09 13:47:04 +0100

    Updated ADXL coded

--- code/testMasterCode/src/main.cpp
+++ code/testMasterCode/src/main.cpp
@@ -4,62 +4,54 @@
 #include <SimpleKalmanFilter.h>
 
 
-// ADXL377 pins
+/// \b GLOBAL \b VARIABLES
 #define ADXL377_XPin 21
 #define ADXL377_YPin 22
 #define ADXL377_ZPin 23
 
-SimpleKalmanFilter FilteredAccADXL377_X = SimpleKalmanFilter(0.05, 0.05, 0.01);
-SimpleKalmanFilter FilteredAccADXL377_Y = SimpleKalmanFilter(0.05, 0.05, 0.01);
-SimpleKalmanFilter FilteredAccADXL377_Z = SimpleKalmanFilter(0.05, 0.05, 0.01);
-int scale = 200; // 3 (±3g) for ADXL337, 200 (±200g) for ADXL377
+SimpleKalmanFilter filteredAccADXL377_X = SimpleKalmanFilter(0.05, 0.05, 0.01);
+SimpleKalmanFilter filteredAccADXL377_Y = SimpleKalmanFilter(0.05, 0.05, 0.01);
+SimpleKalmanFilter filteredAccADXL377_Z = SimpleKalmanFilter(0.05, 0.05, 0.01);
+Vector<double> filteredADXL;
 
-void setup()
-{
-    // Initialize serial communication at 115200 baud
-    Serial.begin(115200);
-}
+float ADXLscale = 200; //  (±200g) for ADXL377
 
-float mapf(float x, float in_min, float in_max, float out_min, float out_max)
-{
-    pinMode(ADXL377_XPin, INPUT);
-    pinMode(ADXL377_YPin, INPUT);
-    pinMode(ADXL377_ZPin, INPUT);
-    //analogReadRes(12);
+
+
+float mapf(float x, float in_min, float in_max, float out_min, float out_max) {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-// Read, scale, and print accelerometer data
-void loop()
-{
+void ADXL377Setup () {
+    pinMode(ADXL377_XPin, INPUT);
+    pinMode(ADXL377_YPin, INPUT);
+    pinMode(ADXL377_ZPin, INPUT);
+}
+
+void getADXL377Acc (Vector<double> returnVect) {
     // Get raw accelerometer data for each axis
     int rawX = analogRead(ADXL377_XPin);
     int rawY = analogRead(ADXL377_YPin);
     int rawZ = analogRead(ADXL377_ZPin);
 
-
-    // Scale accelerometer ADC readings into common units
-    // Scale map depends on if using a 5V or 3.3V microcontroller
-    float scaledX, scaledY, scaledZ; // Scaled values for each axis
-
-    scaledX = mapf(rawX, 0, 1023, -scale, scale);
-    scaledY = mapf(rawY, 0, 1023, -scale, scale);
-    scaledZ = mapf(rawZ, 0, 1023, -scale, scale);
-
-    // Print out raw X,Y,Z accelerometer readings
-    //Serial.print("X: "); Serial.println(rawX);
-    //Serial.print("Y: "); Serial.println(rawY);
-    //Serial.print("Z: "); Serial.println(rawZ);
-    //Serial.println();
-
-    // Print out scaled X,Y,Z accelerometer readings
-    Serial.print("X: "); Serial.print(scaledX); Serial.println(" g");
-    Serial.print("Y: "); Serial.print(scaledY); Serial.println(" g");
-    Serial.print("Z: "); Serial.print(scaledZ); Serial.println(" g");
-    Serial.println();
-
-    delay(100); // Minimum delay of 2 milliseconds between sensor reads (500 Hz)
+    returnVect[0] = mapf(rawX, 0, 1023, -ADXLscale, ADXLscale); // Between 0 and 1023 on 3.3V processor
+    returnVect[1] = mapf(rawY, 0, 1023, -ADXLscale, ADXLscale);
+    returnVect[2] = mapf(rawZ, 0, 1023, -ADXLscale, ADXLscale);
 }
 
-// Same functionality as Arduino's standard map function, except using floats
+void setup() {
+    // Initialize serial communication at 115200 baud
+    Serial.begin(115200);
+    ADXL377Setup();
+
+}
+
+// Read, scale, and print accelerometer data
+void loop() {
+    // TODO: filterting of ADXL
+    Vector<double> ADXLacc = {};
+    getADXL377Acc(ADXLacc);
+    Serial.printf("%lf, %lf, %lf\n", ADXLacc[0], ADXLacc[1], ADXLacc[2]);
+}
+
 
