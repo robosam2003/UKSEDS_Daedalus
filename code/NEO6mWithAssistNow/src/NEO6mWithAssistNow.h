2c27fbfa822a73968cc754e28a3b86c17e55ab37
robos <robosam2003@gmail.com>
2022-05-04 11:01:11 +0100

    added NEO6mWithAssistNow.cpp and NEO6mWithAssistNow.h

---
+++ code/NEO6mWithAssistNow/src/NEO6mWithAssistNow.h
@@ -0,0 +1,64 @@
+//
+// Created by robos on 04/05/2022.
+//
+
+#ifndef NEO6MWITHASSISTNOW_H
+#define NEO6MWITHASSISTNOW_H
+
+// Created by Samuel scott (robosam2003) on 30/04/2022
+// This program sets up the neo6m gps and performs assistNow loading.
+/// Datasheet url: https://content.u-blox.com/sites/default/files/products/documents/u-blox6_ReceiverDescrProtSpec_%28GPS.G6-SW-10018%29_Public.pdf#page=118&zoom=100,0,0
+/// Also see: https://content.u-blox.com/sites/default/files/products/documents/MultiGNSS-Assistance_UserGuide_%28UBX-13004360%29.pdf
+/// little endian format /:)
+
+// This program reads the mgaonline.ubx file from the sd card and then writes it to the NEO6M GPS
+
+
+#include <Arduino.h>
+#include <SD.h>
+#include <TimeLib.h> // : https://github.com/PaulStoffregen/Time
+#include "assistNow.h"
+#include <ctime>
+
+
+
+#define transistorPin 32
+#define ubxHeader1 0xB5
+#define ubxHeader2 0x62
+#define gpsBaudRate 9600
+
+
+/// Change this what Serial port the GPS is connected to
+#define gpsSerial Serial2 // Hardware serial port for GPS
+
+
+time_t getTeensyTime();
+
+void rtcSetup();
+
+time_t TimeFromYMD(int year, int month, int day);
+
+unsigned short GPSweek();
+
+unsigned int actualTimeOfWeekms();
+
+void serialClear(); // gps serial clear hehe
+
+void sendUbx(byte ubxClassID, byte messageID, short payloadLength, const byte payload[]);
+
+void getUbx(byte ubxClassID, byte messageID, short payloadLength, byte payload[]);
+
+int getUbxFromFile(File fptr, byte ubxClassID, byte messageID, short payloadLength, byte payload[]);
+
+void performOnlineAssist();
+
+void NEO6mConfig();
+
+
+
+
+
+
+
+
+#endif //NEO6MWITHASSISTNOW_H
