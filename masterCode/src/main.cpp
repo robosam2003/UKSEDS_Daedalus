99c59696d6d134c8e30f55ac0b4fa9f7d866d065
robos <robosam2003@gmail.com>
2022-06-02 20:39:34 +0100

    Implemented RTC sync in masterCode

--- masterCode/src/main.cpp
+++ masterCode/src/main.cpp
@@ -2,13 +2,16 @@
 #include "C:\Users\robos\CLionProjects\UKSEDS_Daedalus\code\BNO055_forked_and_source\src\BNO055daedalus.h"
 #include "C:\Users\robos\CLionProjects\UKSEDS_Daedalus\code\SDcardDataLog\src\SDcardDataLog.h"
 #include "C:\Users\robos\CLionProjects\UKSEDS_Daedalus\code\NEO6mWithAssistNow\src\NEO6mWithAssistNow.h"
-#include "C:\Users\robos\CLionProjects\UKSEDS_Daedalus\code\RFM96W_Transmitter_LoRa\src\RFM96WtransmitLORA.h"
-#include "C:\Users\robos\CLionProjects\UKSEDS_Daedalus\code\BMP280\src\BMP280.h"
 
+// I have absolutely no idea why just I cant just include the .h file JUST for the transmitter, but hey-ho - quick fix, it works
+#include "C:\Users\robos\CLionProjects\UKSEDS_Daedalus\code\RFM96W_Transmitter_LoRa\src\RFM96WtransmitLORA.h"
+#include "C:\Users\robos\CLionProjects\UKSEDS_Daedalus\code\RFM96W_Transmitter_LoRa\src\RFM96WtransmitLORA.cpp"
+
+#include "C:\Users\robos\CLionProjects\UKSEDS_Daedalus\code\BMP280\src\BMP280.h"
 
 /** Global variables defined in other files:
  * - BNO055daedalus.h
- *   - BNO055 sensor(BNO055_I2C_ADDRESS, &Wire)
+ *   - BNO055 sensor(BNO055_I2C_ADDRESS, &Wire) 
  *   - Vector<double> acc_biases
  *   - Vector<double> gyr_biases
  *   - double prevVect[numDR][9]
@@ -51,25 +54,32 @@
  *
  */
 
+#define RTC_SYNC_BYTE 0x99
+byte statusCode = 0b00000000; // This can be altered to show various stages of the launch.
+#define LAUNCH_DETECTION_MASK 0b00000001
+#define GPS_TURN_ON_MASK 0b00000010
+
+
 void enterToContinue(){
     Serial.printf("Press Enter to continue");
     while (!Serial.available());
+    Serial.println();
     Serial.clear();
 }
 
 void fullSystemTest() {
-    // Tests each subsystem for several seconds.
+    // Tests each subsystem for several seconds. 
+    // Do this just before launch, Use HTERM for terminal input.
     Serial.printf("---------- FULL SYSTEM TEST ----------\n\nPress enter to continue");
-    enterToContinue();
 
     Serial.printf("Timestamp test...\n");
     delay(500);
-    for (int i=0; i<100; i++) {
+    for (int i=0; i<50; i++) {
         uint64_t ts = getTimestampMillis();
         Serial.printf("UNIX TIME: %llu\n", ts);
         delay(100);
     }
-    Serial.printf("Timestamp test complete.\nPress enter to continue.");
+    Serial.printf("Timestamp test complete.\n");
     enterToContinue();
 
     Serial.printf("Testing BNO055 for 15 Seconds... \n");
@@ -84,7 +94,7 @@ void fullSystemTest() {
                       rawBNO055Gyro[0], rawBNO055Gyro[1], rawBNO055Gyro[2]);
         delay(10);
     }
-    Serial.printf("BNO055 test complete.\nPress enter to continue.");
+    Serial.printf("BNO055 test complete.\n");
     enterToContinue();
 
     Serial.printf("Testing ADXL377 for 15 Seconds... \n");
@@ -92,16 +102,36 @@ void fullSystemTest() {
     for (int i=0; i<15*100; i++) {
         Vector<double> rawADXLacc = getADXL377Acc();
         updateADXL377Filters(rawADXLacc);
-        Serial.printf("ADXL377 RAW - acc: (x, y, z) %lf, %lf, %lf\n",
-                      rawADXLacc[0], rawADXLacc[1], rawADXLacc[2]);
+        Serial.printf("ADXL377 FILTERED - acc: (x, y, z) %lf, %lf, %lf\n",
+                      filteredADXL[0], filteredADXL[1], filteredADXL[2]);
         delay(10);
     }
 }
 
+void groundRTCSync() {
+    u_int64_t unixTime = getTimestampMillis();
+    Serial.printf("UNIX TIME: %llu\n", unixTime);
+    byte arr[22];
+    arr[0] = RTC_SYNC_BYTE; // sync byte
+    for (int i=0; i<8; i++) {
+        arr[i+1] = (unixTime >> (7 * 8 - (i * 8))) & 0xFF; // little endian
+    }
+
+    transmitData(arr);
+    Serial.printf("Successfully transmitted UNIX TIME to ground station.\n");
+
+
+
+}
 
 void setup() {
-    delay(500);
     Serial.begin(115200);
+    delay(500);
+
+    enterToContinue();
+
+    Serial.printf("-------------INITIALIZING SENSORS------------\n");
+
 /** Initialise sensors:
      *  - BNO055
      *  - ADXL377
@@ -110,20 +140,45 @@ void setup() {
     BNO055Setup();
     ADXL377Setup();
     BMP280Setup();
-    //NEO6mSetup();
+    NEO6mSetup();
 
-    // * Initialise SD card and transmitter
-    //sdSetup();
-
-    //RFM96WtransmitSetup();
-
+/** Initialise SD card and transmitter */
+    sdSetup();
+    RFM96WtransmitSetup();
     Serial.printf("SUCCESS - All systems initialised.\n");
-    /** Full system test before launch */
+
+/** Send Teensy unix time to ground station over telemetry to sync. */
+    Serial.printf("SYNCING ground station RTC\n");
+    groundRTCSync();
+
+    enterToContinue();
+    
+
+/** Full system test before launch */
     fullSystemTest();
-}
+
+
+
+/** Calibrate sensors (BNO055) and calculate offsets (BNO055)*/
+
+
+/** Wait for "Launch commit" mesasnge from ground station */
+
+
+/** Save current GPS coords to SD card */
+
+
+
+/** Shut down GPS ready for launch */
+
+
+/// We now enter the "ready for launch loop": loop()
+} 
 
 void loop() {
-/*/// Data acquisition - BNO055, ADXL377, BMP280
+
+    
+/// Data acquisition - BNO055, ADXL377, BMP280
     // Get timestamp
     uint64_t timestamp = getTimestampMillis();
 
@@ -135,6 +190,7 @@ void loop() {
     double bmpData[3];
     getBMP280Data(bmpData);
 
+    
 
 
 
@@ -143,6 +199,12 @@ void loop() {
     updateBNOFilters(rawBNO055Gyro, rawBNO055Acc);
     updateADXL377Filters(rawADXLacc);
 
+
+/// Launch detection 
+    // TODO: test launch detection interrupt. 
+    if (!launchInterrupt && (magnitude(filteredAccBNO055) < 2*9.81)) { // TODO: detemine magnitude threshold
+        launchInterrupt = true;
+    }
 /// Calculations
 
 
@@ -164,13 +226,13 @@ void loop() {
     // TODO: Add GPS data under conditions
 
 
-    // TODO: Add Dead reckoning data.
+    // TODO: Add Dead reckoning data. - include orientation!
 
 
 
 /// Data transmission every third cycle
 
 
-/// Ensuring cycles are 10ms long*/
+/// Ensuring cycles are 10ms long
 
 }
